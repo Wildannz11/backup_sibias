f9685f21bf64a5e06a2990e456d3333f78b5534a
wildannz11 <1910631170145@student.unsika.ac.id>
2022-12-08 20:48:52 +0700

    fix cors 2

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ sync();
 
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:3001','http://localhost:3002','http://localhost:3003','http://localhost:3004','http://localhost:3005'], 
+    origin: ['http://localhost:3000','http://localhost:3001','http://localhost:3002','http://localhost:3003','http://localhost:3004','http://localhost:3005'], 
 }));
 
 // app.use(cors());
