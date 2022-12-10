d6046b6a2759256f8bf55498d2470ed63ff597c0
wildannz11 <1910631170145@student.unsika.ac.id>
2022-12-10 23:15:38 +0700

    change verify user

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -104,91 +104,118 @@ router.post('/pemerintah/login', loginPemerintah);
 router.delete('/logout', logout);
 
 // diskusi rakyat mengenai kebijakan
-router.get('/diskusi', verifyUser, getDiskusi);
-router.get('/diskusii', verifyUser, getSearchDiskusibyNama);
+// router.get('/diskusi', verifyUser, getDiskusi);
+// router.get('/diskusii', verifyUser, getSearchDiskusibyNama);
+// // router.get('/diskusi?judul_diskusi=', verifyUser, getSearchDiskusibyNama);
+// router.get('/diskusi/:id', verifyUser, getDiskusiById);
+// router.post('/diskusi', verifyUser, rakyatOnly, createDiskusi);
+// router.patch('/diskusi/:id', verifyUser, rakyatOnly, editDiskusi);
+// router.delete('/diskusi/:id', verifyUser, rakyatOnly, deleteDiskusi);
+
+router.get('/diskusi', getDiskusi);
+router.get('/diskusii', getSearchDiskusibyNama);
 // router.get('/diskusi?judul_diskusi=', verifyUser, getSearchDiskusibyNama);
-router.get('/diskusi/:id', verifyUser, getDiskusiById);
-router.post('/diskusi', verifyUser, rakyatOnly, createDiskusi);
-router.patch('/diskusi/:id', verifyUser, rakyatOnly, editDiskusi);
-router.delete('/diskusi/:id', verifyUser, rakyatOnly, deleteDiskusi);
+router.get('/diskusi/:id', getDiskusiById);
+router.post('/diskusi', rakyatOnly, createDiskusi);
+router.patch('/diskusi/:id', rakyatOnly, editDiskusi);
+router.delete('/diskusi/:id', rakyatOnly, deleteDiskusi);
 
 // CUD chat diskusi
-router.get('/chatdiskusi/:did', verifyUser, getChatDiskusi);
-router.get('/chatdiskusii/:id', verifyUser, getChatDiskusiById);
-router.post('/chatdiskusi/:did', verifyUser, rakyatOnly, createChatDiskusi);
-router.patch('/chatdiskusi/:did/:id', verifyUser, rakyatOnly, editChatDiskusi);
-router.delete('/chatdiskusi/:did/:id', verifyUser, rakyatOnly, deleteChatDiskusi);
+// router.get('/chatdiskusi/:did', verifyUser, getChatDiskusi);
+// router.get('/chatdiskusii/:id', verifyUser, getChatDiskusiById);
+// router.post('/chatdiskusi/:did', verifyUser, rakyatOnly, createChatDiskusi);
+// router.patch('/chatdiskusi/:did/:id', verifyUser, rakyatOnly, editChatDiskusi);
+// router.delete('/chatdiskusi/:did/:id', verifyUser, rakyatOnly, deleteChatDiskusi);
+
+router.get('/chatdiskusi/:did', getChatDiskusi);
+router.get('/chatdiskusii/:id',  getChatDiskusiById);
+router.post('/chatdiskusi/:did', rakyatOnly, createChatDiskusi);
+router.patch('/chatdiskusi/:did/:id', rakyatOnly, editChatDiskusi);
+router.delete('/chatdiskusi/:did/:id', rakyatOnly, deleteChatDiskusi);
 
 // CRUD topic
-router.post('/topic', verifyUser, createTopic);
-router.post('/topicdiskusi', verifyUser, createTopicDiskusi);
+// router.post('/topic', verifyUser, createTopic);
+// router.post('/topicdiskusi', verifyUser, createTopicDiskusi);
 
-router.get('/topic', verifyUser, getTopic);
-router.get('/topicdiskusi', verifyUser, getTopicDiskusi);
-router.get('/topic/:id', verifyUser, getTopicById);
+// router.get('/topic', verifyUser, getTopic);
+// router.get('/topicdiskusi', verifyUser, getTopicDiskusi);
+// router.get('/topic/:id', verifyUser, getTopicById);
 
-router.patch('/topic/:id', verifyUser, editTopic);
-router.patch('/topicdiskusi/:id', verifyUser, editTopicDiskusi);
+// router.patch('/topic/:id', verifyUser, editTopic);
+// router.patch('/topicdiskusi/:id', verifyUser, editTopicDiskusi);
 
-router.delete('/topic/:id', verifyUser, deleteTopic);
-router.delete('/topicdiskusi/:id', verifyUser, deleteTopicDiskusi);
+// router.delete('/topic/:id', verifyUser, deleteTopic);
+// router.delete('/topicdiskusi/:id', verifyUser, deleteTopicDiskusi);
+
+router.post('/topic',  createTopic);
+router.post('/topicdiskusi', createTopicDiskusi);
+
+router.get('/topic',  getTopic);
+router.get('/topicdiskusi', getTopicDiskusi);
+router.get('/topic/:id', getTopicById);
+
+router.patch('/topic/:id', editTopic);
+router.patch('/topicdiskusi/:id', editTopicDiskusi);
+
+router.delete('/topic/:id', deleteTopic);
+router.delete('/topicdiskusi/:id', deleteTopicDiskusi);
 
 // CRUD pemerintah kebijakan
-router.get('/kebijakan', verifyUser, getKebijakan);
-// router.get('/kebijakan?judul_kebijakan=', verifyUser, getSearchKebijakanByNama);
-router.get('/kebijakann', verifyUser, getSearchKebijakanByNama);
-router.get('/kebijakan/:id', verifyUser, getKebijakanById);
-router.post('/kebijakan', verifyUser, pemerintahOnly, createKebijakan);
-router.patch('/kebijakan/:id', verifyUser, pemerintahOnly, editKebijakan);
-router.delete('/kebijakan/:id', verifyUser, pemerintahOnly, deleteKebijakan);
+router.get('/kebijakan', getKebijakan);
+// router.get('/kebijakan?judul_kebijakan=', getSearchKebijakanByNama);
+router.get('/kebijakann', getSearchKebijakanByNama);
+router.get('/kebijakan/:id', getKebijakanById);
+router.post('/kebijakan', pemerintahOnly, createKebijakan);
+router.patch('/kebijakan/:id', pemerintahOnly, editKebijakan);
+router.delete('/kebijakan/:id', pemerintahOnly, deleteKebijakan);
 
-router.patch('/images/kebijakan/:id', verifyUser, pemerintahOnly, editUploadImageKebijakan);
-router.patch('/kebijakan/publish/:id', verifyUser, pemerintahOnly, publishKebijakan);
+router.patch('/images/kebijakan/:id', pemerintahOnly, editUploadImageKebijakan);
+router.patch('/kebijakan/publish/:id', pemerintahOnly, publishKebijakan);
 
 
 // CRUD comment kebijakan
-router.get('/commentkebijakan', verifyUser, getCommentKebijakans);
-router.get('/commentkebijakan/:id', verifyUser, getCommentKebijakansById);
-router.post('/commentkebijakan/:kid', verifyUser, createCommentKebijakans);
-router.patch('/commentkebijakan/:kid/:id', verifyUser, editCommentKebijakans);
-router.delete('/commentkebijakan/:kid/:id', verifyUser, deleteCommentKebijakans);
+router.get('/commentkebijakan', getCommentKebijakans);
+router.get('/commentkebijakan/:id', getCommentKebijakansById);
+router.post('/commentkebijakan/:kid', createCommentKebijakans);
+router.patch('/commentkebijakan/:kid/:id', editCommentKebijakans);
+router.delete('/commentkebijakan/:kid/:id', deleteCommentKebijakans);
 
 
 
 // CRUD tags
-router.post('/tags', verifyUser, createTags);
-router.post('/tagskebijakan', verifyUser, createTagsKebijakan);
+router.post('/tags', createTags);
+router.post('/tagskebijakan', createTagsKebijakan);
 
-router.get('/tags', verifyUser, getTags);
-router.get('/tagskebijakan', verifyUser, getTagsKebijakan);
-router.get('/tags/:id', verifyUser, getTagsById);
+router.get('/tags', getTags);
+router.get('/tagskebijakan', getTagsKebijakan);
+router.get('/tags/:id', getTagsById);
 
-router.patch('/tags/:id', verifyUser, editTags);
-router.patch('/tagskebijakan/:id', verifyUser, editTagsKebijakan);
+router.patch('/tags/:id', editTags);
+router.patch('/tagskebijakan/:id', editTagsKebijakan);
 
-router.delete('/tags/:id', verifyUser, deleteTags);
-router.delete('/tagskebijakan/:id', verifyUser, deleteTagsKebijakan);
+router.delete('/tags/:id', deleteTags);
+router.delete('/tagskebijakan/:id', deleteTagsKebijakan);
 
 
 // CRUD Users
-router.get('/users/:role', verifyUser, rakyatOnly, getUser);
-router.get('/pemerintah/:role', verifyUser, pemerintahOnly, getPemerintah);
+router.get('/users/:role', rakyatOnly, getUser);
+router.get('/pemerintah/:role', pemerintahOnly, getPemerintah);
 
-router.get('/users/:role/:id', verifyUser, rakyatOnly, getUserById);
-router.get('/pemerintah/:role/:id', verifyUser, pemerintahOnly, getPemerintahById);
+router.get('/users/:role/:id', rakyatOnly, getUserById);
+router.get('/pemerintah/:role/:id', pemerintahOnly, getPemerintahById);
 
 router.post('/users', createUser);
 router.post('/pemerintah', createAkunPemerintah);
 
-router.patch('/users/:id',  verifyUser, rakyatOnly, editUser);
-router.patch('/pemerintah/:id',  verifyUser, pemerintahOnly, editPemerintah);
+router.patch('/users/:id', rakyatOnly, editUser);
+router.patch('/pemerintah/:id', pemerintahOnly, editPemerintah);
 
 
-router.delete('/users/:id',  verifyUser, deleteUser);
+router.delete('/users/:id', deleteUser);
 
 // patch user with image 
-router.patch('/images/users/:id', verifyUser, editUploadImageProfile);
-router.get('/images/users/:id', verifyUser, getUserImageById);
+router.patch('/images/users/:id', editUploadImageProfile);
+router.get('/images/users/:id', getUserImageById);
 
 
 
